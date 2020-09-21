139c2c3ef5fa625969d13ef3b3f0c9b0826397c1
daviddeblas <71023594+daviddeblas@users.noreply.github.com>
2020-09-21 19:12:27 -0400

    Update exercice.py
    mise à jour

--- exercice.py
+++ exercice.py
@@ -6,25 +6,53 @@ from typing import List
 
 
 def convert_to_absolute() -> float:
-    return 0.0
+    mon_nombre = float(input('Veuillez saisir un nombre: '))
+    if mon_nombre < 0:
+        valeur_absolue = -mon_nombre
+    else:
+        valeur_absolue = mon_nombre
+
+    return valeur_absolue
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixes = 'JKLMNOP', 'ack'
-
-    return [""]
+    resultat = []
+    for prefixes in prefixes:
+        nom = prefixes + suffixes
+        resultat.append(nom)
+    return resultat
 
 
 def prime_integer_summation() -> int:
-    return 0
-
+    nombre_premier= 0
+    nombre = 2
+    sommes = 0
+    while nombre_premier < 100:
+        est_premier = True
+        for i in range(1, nombre + 1):
+            if (nombre % i == 0 and i != 1 and i != nombre):
+                est_premier = False
+                break
+        if  est_premier:
+                nombre_premier += 1
+                sommes += nombre
+        nombre += 1
+    return sommes
 
 def factorial(number: int) -> int:
-    return 0
+    factorielle = 1
+    for i in range(1, number+1):
+        factorielle = factorielle*i
+    print(factorielle)
+    return factorielle
 
 
 def use_continue() -> None:
-    pass
+    for i in range(1,11):
+        if i==5:
+           continue
+        print(i)
 
 
 def main() -> None:
